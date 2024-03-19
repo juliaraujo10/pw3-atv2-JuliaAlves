ea96f2067fa7260f59b769fcc697c2d22392da06
juliaraujo10 <alvs.july@gmail.com>
2024-03-19 16:46:48 -0300

    Formulario completo

--- pw3-atv2-formulario/src/components/CreateUser.js
+++ pw3-atv2-formulario/src/components/CreateUser.js
@@ -1,9 +1,7 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { cpfMask } from './Mask';
 
 function CreateUser() {
-
     const [name, setName] = useState("");
     const [cpf, setCPF] = useState("");
     const [rg, setRG] = useState("");
@@ -13,85 +11,113 @@ function CreateUser() {
     const [datanasc, setDataNasc] = useState("");
 
     function createUser(event) {
-
         event.preventDefault();
 
-        let user = JSON.stringify({name:name, cpf:cpf, rg:rg, telefone:telefone, telresponsavel:telresponsavel, email:email, datanasc:datanasc});
-        console.log(user); 
+        let user = JSON.stringify({name: name, cpf: cpf, rg: rg, telefone: telefone, telresponsavel: telresponsavel, email: email, datanasc: datanasc});
+        console.log(user);
 
         //AXIOS AQUI!
         axios.post('https://reqres.in/api/users', user)
-            .then((response)=> {
+            .then((response) => {
                 console.log(response);
                 // alert(response)
             })
-            .catch((error)=> {
+            .catch((error) => {
                 console.log(error);
-                alert(error)
+                alert(error);
             });
+    }
 
-            this.state = { documentId: '' }
-            this.handlechange = this.handlechange.bind(this);
-        }
+    function applyCPFFormat(value) {
+        return value
+            .replace(/\D/g, '') 
+            .replace(/(\d{3})(\d)/, '$1.$2') 
+            .replace(/(\d{3})(\d)/, '$1.$2') 
+            .replace(/(\d{3})(\d{1,2})$/, '$1-$2'); 
+    }
 
+    function applyRGFormat(value) {
+        return value
+            .replace(/\D/g, '') 
+            .replace(/(\d{2})(\d)/, '$1.$2') 
+            .replace(/(\d{3})(\d)/, '$1.$2') 
+            .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    }
+
+    function applyTelefoneFormat(value) {
+        return value
+            .replace(/\D/g, '') 
+            .slice(0, 11) 
+            .replace(/^(\d{2})(\d)/g, '($1) $2') 
+            .replace(/(\d{5})(\d)/, '$1-$2'); 
+    }
+
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
 
     return (
-    <form onSubmit={createUser} className='form'>
+        <form onSubmit={createUser} className='form'>
             <h1>FORMULÁRIO ALUNO</h1>
 
             <input
                 type='text'
                 placeholder='DIGITE AQUI SEU NOME'
                 value={name}
-                onChange={(event) =>{setName(event.target.value)}}
+                onChange={(event) => setName(event.target.value)}
             />
 
             <input
-                type="text" 
+                type="text"
                 maxLength='14'
-			    pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" 
                 placeholder='DIGITE AQUI SEU CPF'
-                value={cpf}
-                onChange={(event) =>{setCPF (event.target.value)}}
+                value={applyCPFFormat(cpf)}
+                onChange={(event) => setCPF(event.target.value)}
             />
-            
+
             <input
                 type='text'
+                maxLength='12' 
                 placeholder='DIGITE SEU RG'
-                value={rg}
-                onChange={(event) =>{setRG(event.target.value)}}
+                value={applyRGFormat(rg)}
+                onChange={(event) => setRG(event.target.value)}
             />
 
             <input
                 type='text'
-                placeholder='DIGITE SEU TELEFONE'
-                value={telefone}
-                onChange={(event) =>{setTelefone(event.target.value)}}
+                maxLength='16' 
+                placeholder='DIGITE SEU TELEFONE (COM DDD)'
+                value={applyTelefoneFormat(telefone)}
+                onChange={(event) => setTelefone(event.target.value)}
             />
 
-            
+
             <input
-                type='number'
-                placeholder='DIGITE O TELEFONE DO RESPONSAVEL'
-                value={telresponsavel}
-                onChange={(event) =>{setTelResponsavel(event.target.value)}}
+                type='text'
+                maxLength='16' 
+                placeholder='DIGITE O TELEFONE DO RESPONSÁVEL (COM DDD)'
+                value={applyTelefoneFormat(telresponsavel)}
+                onChange={(event) => setTelResponsavel(event.target.value)}
             />
 
             <input
                 type='text'
                 placeholder='DIGITE AQUI SEU EMAIL'
                 value={email}
-                onChange={(event) =>{setEmail(event.target.value)}}
+                onChange={(event) => setEmail(event.target.value)}
             />
+            {email && !validateEmail(email) && <p style={{color: 'red'}}>Formato de e-mail inválido</p>}
 
             <input
                 type='date'
                 placeholder='DIGITE AQUI SUA DATA DE NASCIMENTO'
                 value={datanasc}
-                onChange={(event) =>{setDataNasc(event.target.value)}}
+                onChange={(event) => setDataNasc(event.target.value)}
             />
             <button type='submit'>CADASTRAR</button>
-    </form>
-)};
+        </form>
+    );
+}
 
 export default CreateUser;
